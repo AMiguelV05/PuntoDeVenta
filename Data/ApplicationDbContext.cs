95db232c13b38362b45205966e646b16a581fc53
Angel Villallvazo <angel.villalvazo5367@alumnos.udg.mx>
2025-11-07 13:40:52 -0600

    Actualiza el menú de navegación y elimina componentes innecesarios; agrega gestión de clientes y mejora la estructura de datos.

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -10,4 +10,10 @@ public class ApplicationDbContext : DbContext
     public DbSet<Categoria> Categorias { get; set; }
     public DbSet<Venta> Ventas { get; set; }
     public DbSet<DetalleVenta> DetallesVenta { get; set; }
+    public DbSet<Empleado> Empleados { get; set; }
+    public DbSet<Entrada> Entradas { get; set; }
+    public DbSet<DetalleEntrada> DetallesEntrada { get; set; }
+    public DbSet<Factura> Facturas { get; set; }
+    public DbSet<Cliente> Clientes { get; set; }
+    public DbSet<Proveedor> Proveedores { get; set; }
 }
